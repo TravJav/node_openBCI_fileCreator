175a1ba2c4db7739e56c7d8fa811e72ad371d737
TravJav <travishaycock92@gmail.com>
2017-09-11 00:36:33 -0400

    send Json String

--- connect.js
+++ connect.js
@@ -1,8 +1,17 @@
 var net = require('net');
+const Buffer = require('buffer').Buffer;
+var json = require('json');
 
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
+var server = net.createServer(function (socket) {
+	console.log(' \n CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+	
+
+
+
+	var obj = { "name":"John", "age":30, "city":"New York"};
+	var myJSON = JSON.stringify(obj)
+	socket.write(myJSON);
 	socket.pipe(socket);
 });
 
-server.listen(4007, '127.0.0.1');
+server.listen(6008, '127.0.0.1');
