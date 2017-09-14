3641350d3f04362c227343ab691cad85d2734ba1
TravJav <travishaycock92@gmail.com>
2017-09-13 22:28:10 -0400

    before different approach

--- connect.js
+++ connect.js
@@ -3,77 +3,80 @@ const Buffer = require('buffer').Buffer;
 var json = require('json');
 const _ = require('lodash');
 const Ganglion = require('openbci-ganglion').Ganglion;
-const ganglion = new  Ganglion();
+const ganglion = new Ganglion();
+const PORT = 8500;
 
 
-// fix this to make socket global
+
+
 var server = net.createServer(function (socket) {
-	console.log(' \n CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
-  ganglion.searchStart(); // start searching for the ganglion
-  console.log("Finished Searching For BLE");
+	//console.log(' \n CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+	ganglion.searchStart(); // start device search with ble
+
+       pass_data(socket);
+
+});
+
+server.listen(8656, '127.0.0.1');
+
+server.on('listening',function(){
+    console.log('Server Is Running');
+});
+
+
+
+
+
+
+ganglion.once('ganglionFound', (peripheral) => {
+	// Stop searching for BLE devices once a ganglion is found.
+	 // start searching for the ganglion
+	ganglion.searchStop();
+	/*
+	Use throttle to control the sample rate curently set for .2 of a second
+	*/
+	ganglion.on('sample', _.throttle(sample => {
+		/** Work with sample */
+
+
+		const transferdata = [];// array to hold our data 
+		for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+			GanglionObjects = sample.channelData[i].toFixed(8);
+			transferdata.push(GanglionObjects);
+
+		}
+
+		pass_data(transferdata);
+	}, 200, { leading: true })) /// .2 of a second
+
+
+	ganglion.once('ready', () => {
+		ganglion.streamStart();
+	});
+
+	ganglion.connect(peripheral);
 
 });
 
 
+
+
+
 /*
-Once the device has been found
+Function to actually pass our data to the client side in C++
 
-*/ 
+*/
+function pass_data(transferdata, socket) {
 
-ganglion.once('ganglionFound', (peripheral) => {
-	// Stop searching for BLE devices once a ganglion is found.
-	ganglion.searchStop();
-	
-	
-	/*
-	Use throttle to control the sample rate
-	*/
-	ganglion.on('sample', _.throttle(sample => {
-	/** Work with sample */
-	
-	
-	const transferdata = [];
-		for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-			GanglionObjects = sample.channelData[i].toFixed(8);
-			transferdata.push(GanglionObjects);
-		   
-		}
-	
-		pass_data(transferdata);    
-	  }, 200, {leading:true}))
-		
-	
-	
-	
-	ganglion.once('ready', () => {
-	  ganglion.streamStart();
-	  });
-	
-	  ganglion.connect(peripheral);
-	
-	});
-
-
-
-
-
-
-	function pass_data(transferdata){
-		
-
-		var hardware_data = JSON.stringify(transferdata)
-		socket.write(hardware_data);
+		console.log(transferdata)
+		var hardware_data = JSON.stringify(transferdata) // we need  put the data in JSON first 
+		socket.write(hardware_data); // send the data out in the socket itself
 		socket.pipe(socket);
-		
-		}
+	
+}
 
 
 
 
 
 
-
-
-
-// Listen for client C++ Ganglion application
-server.listen(6003, '127.0.0.1');
