61901398c0b1a96e40449d5d8faa5206865b2a51
TravJav <travishaycock92@gmail.com>
2017-09-16 21:16:27 -0400

    no prompt implemented

--- connect.js
+++ connect.js
@@ -6,42 +6,46 @@ const Ganglion = require('openbci-ganglion').Ganglion;
 const ganglion = new Ganglion();
 
 
-ganglion.searchStart(); // start device search with ble
-ganglion.once('ganglionFound', (peripheral) => {
-	// Stop searching for BLE devices once a ganglion is found.
-	// start searching for the ganglion
-	ganglion.searchStop();
-	/*
-	Use throttle to control the sample rate curently set for .2 of a second
-	*/
-	ganglion.on('sample', (sample => {
-		/** Work with sample */
-
-
-		const transferdata = [];// array to hold our data 
-		for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-			GanglionObjects = sample.channelData[i].toFixed(8);
-			transferdata.push(GanglionObjects);
-
-		}
-
-	   // write data
-      
-	   fs.appendFileSync("/Users/travishaycock/Desktop/data.csv", transferdata );
+var FILENAME = "/Users/travishaycock/Desktop/exam.csv";
 
 
 
+		
+	ganglion.searchStart(); // start device search with ble
+	ganglion.once('ganglionFound', (peripheral) => {
+		// Stop searching for BLE devices once a ganglion is found.
+		// start searching for the ganglion
+		ganglion.searchStop();
+		/*
+		Use throttle to control the sample rate curently set for .2 of a second
+		*/
+		ganglion.on('sample', (sample => {
+			/** Work with sample */
 
-	}));
+
+			const transferdata = [];// array to hold our data 
+			for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+				GanglionObjects = sample.channelData[i].toFixed(8);
+				transferdata.push(GanglionObjects);
+
+			}
 
 
-	ganglion.once('ready', () => {
-		ganglion.streamStart();
+			console.log(transferdata)
+			fs.appendFileSync(FILENAME, transferdata);
+
+
+
+
+		}));
+
+
+		ganglion.once('ready', () => {
+			ganglion.streamStart();
+		});
+
+		ganglion.connect(peripheral);
+
 	});
-
-	ganglion.connect(peripheral);
-
-});
-
 
 
